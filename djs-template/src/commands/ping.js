47f475c20a6e2e97fb191fc97ec114642104ff14
Bur4K <145892535+Bur4kt@users.noreply.github.com>
2024-04-25 22:35:52 +0300

    .

--- djs-template/src/commands/ping.js
+++
@@ -1,8 +0,0 @@
-const { SlashCommandBuilder } = require("discord.js");
-module.exports = {
-  name: "ping",
-  description: "Replies with Pong!",
-  async execute(interaction, client) {
-    await interaction.reply("Pong!");
-  },
-};
